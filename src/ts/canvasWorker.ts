a66c4a012338167ca78c7d4d56a914efd2408748
NathanWanjongkhum <wanjongkhumnathan@gmail.com>
2023-01-17 20:44:41 -0800

    Created offscreen canvas with worker
    Performance update that puts heavy computation and painting off thread.

--- src/ts/canvasWorker.ts
+++ src/ts/canvasWorker.ts
@@ -3,63 +3,74 @@ export type {};
 
 import { Client, Construction, Crafter, Entity, Mover } from "./entities";
 
-const interval = 1 / 120;
+let canvas: OffscreenCanvas;
+let ctx: OffscreenCanvasRenderingContext2D;
+
 const PI2 = 2 * Math.PI;
 
 self.onmessage = async (msg) => {
-  const { canvas, dpi, entities } = msg.data;
+  const { _canvas, _dimensions, _entities } = msg.data;
 
-  const ctx = canvas.getContext("2d");
+  if (_canvas) {
+    canvas = _canvas;
+    ctx = canvas.getContext("2d")! as OffscreenCanvasRenderingContext2D;
 
-  const img = await createImageBitmap(
-    canvas,
-    0,
-    0,
-    canvas.width,
-    canvas.height,
-    {
-      resizeWidth: canvas.width * dpi,
-      resizeHeight: canvas.height * dpi,
-      resizeQuality: "high",
-    }
-  );
+    fixSize(_dimensions);
+  }
+  if (_dimensions) {
+    fixSize(_dimensions);
+  }
+
+  if (!ctx || !_entities) return;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.beginPath();
   ctx.fillStyle = "#FFFFFF";
 
-  ctx.rect(100, 100, 500, 500);
+  const movers: Mover[] = _entities["movers"];
+  batchUpdateMovers(movers);
 
-  //   batchUpdateMovers();
-  //   batchRenderCrafters();
+  const crafters: Crafter[] = _entities["crafters"];
+  batchRenderCrafters(crafters);
 
   ctx.fill();
-  const imgData = ctx.getImageData(0, 0, img.width, img.height);
-
-  ctx.putImageData(imgData, 0, 0);
-
-  function batchUpdateMovers() {
-    let _x: number;
-    let _y: number;
-
-    (entities["movers"] as Mover[]).forEach((mover) => {
-      //   mover.update(interval);
-
-      _x = Math.round(mover.pos.x);
-      _y = Math.round(mover.pos.y);
-
-      ctx.moveTo(_x + 5, _y);
-      ctx.arc(_x, _y, 5, 0, PI2);
-    });
-  }
-  function batchRenderCrafters() {
-    (entities["crafters"] as Crafter[]).forEach((crafter) => {
-      ctx.moveTo(crafter.pos.x + 50, crafter.pos.y);
-      ctx.rect(crafter.pos.x, crafter.pos.y, 50, 50);
-      // ctx.arc(crafter.pos.x, crafter.pos.y, crafter.range, 0, PI2);
-    });
-  }
 
   self.postMessage({ text: "worker" });
 };
+
+function batchUpdateMovers(movers: Mover[]) {
+  if (!movers) return;
+
+  let _x: number;
+  let _y: number;
+
+  movers.forEach((mover) => {
+    _x = Math.round(mover.pos.x);
+    _y = Math.round(mover.pos.y);
+
+    ctx.moveTo(_x + 5, _y);
+    ctx.arc(_x, _y, 5, 0, PI2);
+  });
+}
+function batchRenderCrafters(crafters: Crafter[]) {
+  if (!crafters) return;
+
+  crafters.forEach((crafter) => {
+    ctx.moveTo(crafter.pos.x + 50, crafter.pos.y);
+    ctx.rect(crafter.pos.x, crafter.pos.y, 50, 50);
+    // ctx.arc(crafter.pos.x, crafter.pos.y, crafter.range, 0, PI2);
+  });
+}
+
+interface canvasDimensions {
+  width: number;
+  height: number;
+  dpi: number;
+}
+
+function fixSize(dimensions: canvasDimensions) {
+  canvas.width = Math.floor(dimensions.width * dimensions.dpi);
+  canvas.height = Math.floor(dimensions.height * dimensions.dpi);
+  ctx.scale(dimensions.dpi, dimensions.dpi);
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,6 +8,7 @@ let canvas: HTMLCanvasElement;
 let offscreenCanvas: OffscreenCanvas;
 
 let dpi = window.devicePixelRatio;
+let canvasDimensions: { width: number; height: number; dpi: number };
 
 // Lists
 let entities: Record<string, Entity[]> = {
@@ -17,36 +18,51 @@ let entities: Record<string, Entity[]> = {
 };
 
 // Cache
+const interval = 1 / 120;
 
 export function init(canvas_: HTMLCanvasElement) {
   canvas = canvas_;
-  // ctx = canvas.getContext("2d")!;
 
   offscreenCanvas = canvas.transferControlToOffscreen();
 
-  // fix_dpi();
-
   canvas.addEventListener("mousemove", onMouseMove, false);
+  window.addEventListener("resize", fixSize, false);
 
   initCrafter("processing unit", 500, 500);
   initCrafter("processing unit", 1000, 800);
 
-  for (let i = 0; i < 500; i++) {
-    initMover(400 + ((i * 10) % 100), 50 + ((i * 10) % 900));
+  for (let i = 0; i < 100; i++) {
+    initMover(400 + i * 10, 50 + ((i * 10) % 300));
 
     const mover = (entities["movers"] as Mover[])[i];
 
-    mover.tasks[0] = {
-      client: entities.crafters[0] as Client,
-      order: "recieve",
-    };
-    mover.tasks[1] = {
-      client: entities.crafters[1] as Client,
-      order: "deposit",
-    };
+    if (!mover) return;
+    // mover.tasks[0] = {
+    //   client: entities.crafters[0] as Client,
+    //   order: "recieve",
+    // };
+    // mover.tasks[1] = {
+    //   client: entities.crafters[1] as Client,
+    //   order: "deposit",
+    // };
 
     mover.setTask(0);
   }
+
+  canvasDimensions = {
+    width: canvas.clientWidth,
+    height: canvas.clientHeight,
+    dpi: dpi,
+  };
+
+  worker.postMessage(
+    {
+      _canvas: offscreenCanvas,
+      _dimensions: canvasDimensions,
+      _entities: entities,
+    },
+    [offscreenCanvas]
+  );
 
   requestAnimationFrame(tick);
 }
@@ -65,16 +81,33 @@ function initMover(x: number, y: number) {
 }
 
 function tick() {
+  update();
   render();
 
-  // requestAnimationFrame(tick);
+  requestAnimationFrame(tick);
+}
+
+function update() {
+  (entities["movers"] as Mover[]).forEach((mover) => {
+    mover.update(interval);
+  });
 }
 
 function render() {
-  worker.postMessage(
-    { canvas: offscreenCanvas, dpi: dpi, entities: entities },
-    [offscreenCanvas]
-  );
+  worker.postMessage({ _entities: entities });
 }
 
 function onMouseMove(e: Event) {}
+
+function fixSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  canvasDimensions = {
+    width: canvas.clientWidth,
+    height: canvas.clientHeight,
+    dpi: dpi,
+  };
+
+  worker.postMessage({ _dimensions: canvasDimensions });
+}
